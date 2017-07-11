0209afb1b29580845df62bb5ce4d0f7db81b4ae7
GuoYuanfang <guo.yuanfang@yahoo.com>
2017-07-11 20:43:08 +0800

    windowsForm3
    画图

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart3 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(49, 29);
+            this.chart1.Name = "chart1";
+            series3.ChartArea = "ChartArea1";
+            series3.Legend = "Legend1";
+            series3.Name = "Series1";
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(575, 175);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // chart2
+            // 
+            chartArea4.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea4);
+            legend4.Name = "Legend1";
+            this.chart2.Legends.Add(legend4);
+            this.chart2.Location = new System.Drawing.Point(262, 282);
+            this.chart2.Name = "chart2";
+            series4.ChartArea = "ChartArea1";
+            series4.Legend = "Legend1";
+            series4.Name = "Series1";
+            this.chart2.Series.Add(series4);
+            this.chart2.Size = new System.Drawing.Size(1107, 634);
+            this.chart2.TabIndex = 1;
+            this.chart2.Text = "chart2";
+            this.chart2.Click += new System.EventHandler(this.chart2_Click);
+            // 
+            // chart3
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart3.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart3.Legends.Add(legend5);
+            this.chart3.Location = new System.Drawing.Point(1063, 1052);
+            this.chart3.Name = "chart3";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart3.Series.Add(series5);
+            this.chart3.Size = new System.Drawing.Size(853, 423);
+            this.chart3.TabIndex = 2;
+            this.chart3.Text = "chart3";
+            this.chart3.Click += new System.EventHandler(this.chart3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2495, 1501);
+            this.Controls.Add(this.chart3);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart3;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms.DataVisualization;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            // 设置曲线的样式
+            
+            Series series = chart1.Series[0];
+            // 画样条曲线（Spline）
+            series.ChartType = SeriesChartType.Spline;
+            // 线宽2个像素
+            series.BorderWidth = 2;
+            // 线的颜色：红色
+            series.Color = System.Drawing.Color.Red;
+            // 图示上的文字
+            series.LegendText = "大宗商品物价走势";
+
+            // 准备数据 
+            List<double> values = new List<double> { 95, 30, 20, 23, 60, 87, 42, 77, 92, 51, 29 };
+
+            // 在chart中显示数据
+            int x = 0;
+            foreach (double v in values)
+            {
+                series.Points.AddXY(x, v);
+                x++;
+            }
+
+            // 设置显示范围
+            ChartArea chartArea = chart1.ChartAreas[0];
+            chartArea.AxisX.Minimum = 0;
+            chartArea.AxisX.Maximum = 10;
+            chartArea.AxisY.Minimum = 0d;
+            chartArea.AxisY.Maximum = 100d;
+        }
+
+        private void chart2_Click(object sender, EventArgs e)
+        {
+            Random ra = new Random();
+
+            Series se = chart2.Series[0];
+            se.ChartType = SeriesChartType.Bar;
+            se.BorderColor = System.Drawing.Color.DarkBlue;
+            se.LegendText = "test chart";
+
+
+
+            for(int i = 0; i < 10; i++)
+            {
+                se.Points.AddXY(i, ra.NextDouble());
+
+            }
+            ChartArea CA = chart2.ChartAreas[0];
+            CA.AxisY.Minimum = 0d;
+            CA.AxisY.Maximum = 1d;
+
+        }
+
+        private void chart3_Click(object sender, EventArgs e)
+        {
+            
+            Random ran = new Random();
+            Series ss = new Series();
+            ss.ChartType = SeriesChartType.Candlestick;
+            ss.BorderWidth = 2;
+
+            ss.Color = System.Drawing.Color.Blue;
+            ss.BorderColor = System.Drawing.Color.Red;
+           
+            
+
+            ss.LegendText = "大宗商品物价走势";
+
+            List<double> low = new List<double>();//最低
+
+            List<double> high = new List<double>();//最高
+
+            List<double> start = new List<double>();//开盘
+
+            List<double> end = new List<double>();//收盘
+
+
+            List<DataPoint> dp = new List<DataPoint>();
+
+            for (int i = 0; i < 7; i++)
+            {
+               low.Add( 10 * ran.NextDouble());
+                high.Add(low[i] + 2* ran.NextDouble());
+                start.Add(low[i] + ran.NextDouble() * (high[i] - low[i]));
+                end.Add(low[i] + ran.NextDouble() * (high[i] - low[i]));
+                             
+
+            }
+
+            
+            for (int i = 0; i < 7; i++)
+            {
+                double[] t = { high[i], low[i], start[i], end[i] }; // K线图点的纵坐标为一个double数组
+                DataPoint temp = new DataPoint(i, t);
+                ss.Points.Add(temp);
+            }
+
+            ChartArea caa = chart3.ChartAreas[0];
+           
+
+        }
+    }
+}
